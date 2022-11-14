1f66485723ae248ec26a11aacdadcc024cbed671
pongdang <pongpong0121@kakao.com>
2022-11-15 01:05:33 +0900

    ⭐ feat : react-query 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,35 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/GlobalStyle';
 import { MainLayout } from '../components/MainLayout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ErrorBoundary, RenderFallbackProps } from '../components/error/ErrorBoundary';
+import { TokenContextProvider } from '../context/TokenContext';
+import { ErrorAlert } from '../components/ErrorAlert';
+
+const queryClient = new QueryClient();
+
+function RenderFallback({ error, children, reset }: RenderFallbackProps) {
+  return (
+    <>
+      <ErrorAlert errorMessage={error.message} reset={reset} />
+      {children}
+    </>
+  );
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <ErrorBoundary renderFallback={RenderFallback}>
+        <QueryClientProvider client={queryClient}>
+          <TokenContextProvider>
+            <GlobalStyle />
+            <MainLayout>
+              <Component {...pageProps} />
+            </MainLayout>
+          </TokenContextProvider>
+        </QueryClientProvider>
+      </ErrorBoundary>
     </>
   );
 }
