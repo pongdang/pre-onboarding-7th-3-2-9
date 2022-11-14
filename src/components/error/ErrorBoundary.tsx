5416dd7c2de1f5c4d137ae4fd77e0f3f24b77ff7
pongdang <pongpong0121@kakao.com>
2022-11-15 01:05:07 +0900

    ⭐ feat : 에러바운더리 컴포넌트 생성

---
+++ src/components/error/ErrorBoundary.tsx
@@ -0,0 +1,69 @@
+import { Component, ReactNode } from 'react';
+import { EventErrorHandler } from './EventErrorHandler';
+
+export interface RenderFallbackProps {
+  error: Error;
+  reset: () => void;
+  children?: ReactNode;
+}
+
+// eslint-disable-next-line no-unused-vars
+type RenderFallback = ({ error, reset, children }: RenderFallbackProps) => ReactNode;
+
+interface Props {
+  children?: ReactNode;
+  renderFallback: RenderFallback;
+}
+
+interface State {
+  error?: Error;
+}
+
+// NOTE: ErrorBoundary 는 클래스 컴포넌트로만 작성할 수 있어 부득이하게 클래스 컴포넌트를 이용합니다.
+export class OriginalErrorBoundary extends Component<Props, State> {
+  static getDerivedStateFromError(error: Error): State {
+    return { error };
+  }
+
+  constructor(props: Props) {
+    super(props);
+    this.state = { error: undefined };
+  }
+
+  resetErrorBoundary = () => {
+    this.setState({ error: undefined });
+  };
+
+  componentDidCatch(error: Error) {
+    this.setState({ error });
+  }
+
+  render() {
+    const { error } = this.state;
+    const { renderFallback, children } = this.props;
+
+    // eslint-disable-next-line eqeqeq
+    if (error != null) {
+      return renderFallback({ error, reset: this.resetErrorBoundary });
+    }
+
+    return children;
+  }
+}
+
+export function ErrorBoundary({
+  children,
+  ...props
+}: {
+  children?: ReactNode;
+  renderFallback: RenderFallback;
+}) {
+  return (
+    <OriginalErrorBoundary
+      {...props}
+      renderFallback={({ error, reset }) => props.renderFallback({ error, children, reset })}
+    >
+      <EventErrorHandler>{children}</EventErrorHandler>
+    </OriginalErrorBoundary>
+  );
+}
