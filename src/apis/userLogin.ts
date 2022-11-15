cf64a24dec8c1f9e1f4b550725bc554ae05bfabc
pongdang <pongpong0121@kakao.com>
2022-11-15 11:14:28 +0900

    👏 chore: 로그인 함수 이름 변경

--- src/apis/userLogin.ts
+++ src/apis/userLogin.ts
@@ -1,9 +1,9 @@
-import { post } from '../axiosInstance';
+import { post } from './axiosInstance';
 interface FetchedUser {
   user: { email: string; id: number };
   accessToken: string;
 }
-export const fetchUser = async ({ email, password }: { email: string; password: string }) => {
+export const userLogin = async ({ email, password }: { email: string; password: string }) => {
   const { data } = await post<FetchedUser>({
     url: '/api/login',
     data: { email, password },
