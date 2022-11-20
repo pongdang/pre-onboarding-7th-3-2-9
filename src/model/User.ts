e40b2bc9c1741e176ee9de7b088c9eac3c4ea965
pongdang <pongpong0121@kakao.com>
2022-11-20 20:50:09 +0900

    feat: account interface 작성

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,4 +1,14 @@
-export interface User {
-  email: string;
-  password: string;
+export interface UserAccount {
+  id: number;
+  user_id: number;
+  uuid: string;
+  broker_id: string;
+  status: number;
+  number: string;
+  name: string;
+  assets: string;
+  payments: string;
+  is_active: boolean;
+  created_at: string;
+  updated_at: string;
 }
